32d747df98378e9a31623c5cc473e33fa20aafab
ue68860 <silvan.bucher@iflow.ch>
2017-05-30 16:29:00 +0200

    Silvan's Version

--- src/ch/iflow/devday2017/functions/FunctionsMain.java
+++ src/ch/iflow/devday2017/functions/FunctionsMain.java
@@ -1,7 +1,10 @@
 // $Id$
 package ch.iflow.devday2017.functions;
 
+import java.util.Comparator;
 import java.util.List;
+
+import ch.iflow.devday2017.Presentation;
 
 public class FunctionsMain {
     
@@ -9,15 +12,22 @@ public class FunctionsMain {
         List<Car> cars = Car.makeCars();
         
         // implement exercise 1 here...
-        
-        Car.printCars("1. Übung - günstige rote Audis", cars);
+        List<Car> cars1 = Presentation.filterCars(cars, car -> car.getColor().equals(Car.Color.RED) && car.getPrice() < 10000);
+        Car.printCars("1. Übung - günstige rote Audis", cars1);
         
         // implement exercise 2 here...
-        
+        cars.sort(Comparator.comparing(Car::getPrice).reversed()); 
         Car.printCars("2. Übung - Sortierung nach Preis", cars);
         
         // implement exercise 3 here...
-        
+        cars.sort((Car car1, Car car2) -> {
+        	int result = car1.getManufacturer().compareTo(car2.getManufacturer());
+        	return result != 0 ? result : car1.getModel().compareTo(car2.getModel());
+        });
         Car.printCars("3. Übung - Sortierung nach Hersteller und Modell", cars);
+        
+        // implement exercise 3.1 here...
+        cars.sort(Comparator.comparing(Car::getManufacturer).thenComparing(Car::getModel).reversed());
+        Car.printCars("3.1 Übung - Sortierung nach Hersteller und Modell", cars);
     }
 }
--- src/ch/iflow/devday2017/streams/StreamsMain.java
+++ src/ch/iflow/devday2017/streams/StreamsMain.java
@@ -1,8 +1,12 @@
 // $Id$
 package ch.iflow.devday2017.streams;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import ch.iflow.devday2017.functions.Car;
 
@@ -13,17 +17,17 @@ public class StreamsMain {
         int[] numbersArray = {4, 9, 10, 3, 17, 0, 3, 2, 8, 15, 9, 11, 1, 3, 20};
         
         // implement exercise 1 here...
-        int priceSumOfThreeOldestCars = 0;
-        
-        System.out.println("1. Übung - Preis-Summe der 3 ältesten Autos: " + priceSumOfThreeOldestCars);
+        List<Car> cars2 = cars.stream().sorted(Comparator.comparing(Car::getYear)).limit(3).collect(Collectors.toList());
+        System.out.println("1. Übung - Preis-Summe der 3 ältesten Autos: " + cars2.stream().mapToInt(Car::getPrice).sum());
         
         // implement exercise 2 here...
-        int sumsOfNumbersSmallerThan10 = 0;
-        
+        int sumsOfNumbersSmallerThan10 = Arrays.stream(numbersArray).filter(num -> num < 10).sum();
         System.out.println("2. Übung - Summe aller Zahlen kleiner 10: " + sumsOfNumbersSmallerThan10);
 
         // implement exercise 3 here...
-        List<Car> selectedCars = Collections.emptyList();
+        List<Car> selectedCars = cars.stream().filter(car -> !car.getManufacturer().equals("Toyota")).collect(Collectors.toList());
+        selectedCars = selectedCars.stream().sorted(Comparator.comparing(Car::getYear)).limit(selectedCars.size() - 3).collect(Collectors.toList());
+        selectedCars = selectedCars.stream().sorted(Comparator.comparing(Car::getPrice)).collect(Collectors.toList());
         
         Car.printCars("3. Übung - Autos sortiert nach Preis, ohne die 3 neuesten und ohne Toyotas: ", selectedCars);
     }
